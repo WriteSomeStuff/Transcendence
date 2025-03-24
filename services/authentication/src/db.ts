bc0662627c9bd7b61613acc2bea34d203ad20613
Milana <94888u@gmail.com>
2025-03-24 21:38:02 +0100

    update authentication as gateway

--- services/authentication/src/db.ts
+++ services/authentication/src/db.ts
@@ -1,14 +1,34 @@
 import Database from 'better-sqlite3';
-import BetterSqlite3 from 'better-sqlite3';
+import type BetterSqlite3 from 'better-sqlite3';
 
-const db: BetterSqlite3.Database = new Database('database.sqlite3');
+const DB_PATH: string = 'database.sqlite3';
 
-db.exec(`
-    CREATE TABLE IF NOT EXISTS users (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      username TEXT UNIQUE NOT NULL,
-      password TEXT NOT NULL
-    )
-  `)
+class DatabaseService {
+  private static instance: BetterSqlite3.Database;
+
+  private constructor() {}
+
+  public static getInstance(): BetterSqlite3.Database {
+    if (!DatabaseService.instance) {
+      DatabaseService.instance = new Database(DB_PATH, {verbose: console.log});
+      try {
+        DatabaseService.instance.exec(`
+        CREATE TABLE IF NOT EXISTS users (
+          id TEXT PRIMARY KEY,
+          username TEXT UNIQUE NOT NULL,
+          password TEXT NOT NULL
+        )
+        `);
+        console.log('Users table exists!');
+      }
+      catch (e) {
+        console.error('Error creating users table', e);
+      }
+    }
+    return DatabaseService.instance;
+  }
+}
+
+const db: BetterSqlite3.Database = DatabaseService.getInstance();
 
 export default db;
--- services/authentication/src/server.ts
+++ services/authentication/src/server.ts
@@ -1,12 +1,19 @@
 import fastify from "fastify";
-import jwt, {Secret} from '@fastify/jwt';
 import bcrypt from "bcrypt";
+import {v6 as uuidv6} from 'uuid';
+import jwt from "@fastify/jwt";
+import proxy from "@fastify/http-proxy"
 
 import db from "./db.ts";
 
+const select = db.prepare("SELECT * FROM users WHERE username = ?");
+const insert = db.prepare(
+    'INSERT into users (id, username, password) VALUES (@id, @username, @password)'
+);
+
 const app = fastify();
 
-app.register(jwt, {secret: process.env.JWT_SECRET as Secret});
+app.register(jwt, {secret: process.env.JWT_SECRET as string});
 
 app.post("/register", async (req, res) => {
     const { username, password } = req.body as { username: string; password: string };
@@ -17,12 +24,66 @@ app.post("/register", async (req, res) => {
 
     const hashedPassword = await bcrypt.hash(password, 10); // TODO possibly replace with own hash calculation
 
-    db.prepare(`INSERT INTO users (username, password) VALUES (?, ?)`).run(username, hashedPassword);
+    const checkedInsert = db.transaction((username: string, password: string) => {
+        if (select.get(username) !== undefined) {
+            return res.status(400).send({error: "Username must be unique"});
+        }
+        let id = uuidv6();
+        insert.run({id, username, password});
+        return res.status(201).send("User created!");
+    });
+    try {
+        return checkedInsert(username, hashedPassword);
+    }
+    catch (error) {
+        return res.status(500).send('Unknown error');
+    }
 });
+
+app.post("/login", async (req, res) => {
+    const { username, password } = req.body as { username: string; password: string };
+
+    if (!username || !password) {
+        return res.status(400).send({ error: "Username and password is required"});
+    }
+    const user = select.get(username) as { id: string; username: string; password: string };
+    if (!user) {
+        return res.status(404).send({ error: "User not found" });
+    }
+    if (!(await bcrypt.compare(password, user.password))) {
+        return res.status(401).send({ error: "Password is incorrect" });
+    }
+    const token = app.jwt.sign({ id: user.id, type: 'registered' });
+    return res.status(200).send({ token: token });
+})
+
+app.post("/guest", async (req, res) => {
+    try {
+        await req.jwtVerify();
+        res.status(400).send({ error: "Already authenticated" });
+    }
+    catch (error) {
+        const token = app.jwt.sign({ id: uuidv6(), type: 'guest' });
+        return res.status(200).send({ token: token });
+    }
+})
 
 app.get("/health", (req, res) => {
     res.send({message: "Success"});
 });
+
+app.addHook("onRequest", async (req, res) => {
+    if (["/health", "/login", "/register", "/guest"].includes(req.url))
+        return;
+    try {
+        await req.jwtVerify();
+    }
+    catch (error) {
+        res.status(401).send({ error: "Unauthorized" });
+    }
+})
+
+app.register(proxy, { upstream: 'http://localhost:8545' }); // TODO replace with some normal value
 
 app.listen({port: 8080, host: '0.0.0.0'}, (err: Error | null, address: string) => {
     if (err) {
