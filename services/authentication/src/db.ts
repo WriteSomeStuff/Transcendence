ba3821a4310768d275dcba6950d1b9ed631dce3f
Milana <94888u@gmail.com>
2025-03-24 22:45:40 +0100

    lint & format authentication service

--- services/authentication/src/db.ts
+++ services/authentication/src/db.ts
@@ -1,7 +1,7 @@
-import Database from 'better-sqlite3';
-import type BetterSqlite3 from 'better-sqlite3';
+import Database from "better-sqlite3";
+import type BetterSqlite3 from "better-sqlite3";
 
-const DB_PATH: string = 'database.sqlite3';
+const DB_PATH: string = "database.sqlite3";
 
 class DatabaseService {
   private static instance: BetterSqlite3.Database;
@@ -10,7 +10,9 @@ class DatabaseService {
 
   public static getInstance(): BetterSqlite3.Database {
     if (!DatabaseService.instance) {
-      DatabaseService.instance = new Database(DB_PATH, {verbose: console.log});
+      DatabaseService.instance = new Database(DB_PATH, {
+        verbose: console.log,
+      });
       try {
         DatabaseService.instance.exec(`
         CREATE TABLE IF NOT EXISTS users (
@@ -19,10 +21,9 @@ class DatabaseService {
           password TEXT NOT NULL
         )
         `);
-        console.log('Users table exists!');
-      }
-      catch (e) {
-        console.error('Error creating users table', e);
+        console.log("Users table exists!");
+      } catch (e) {
+        console.error("Error creating users table", e);
       }
     }
     return DatabaseService.instance;
