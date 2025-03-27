b8a5b37ccfd777b29728dc483a16471975d9b053
Milana <94888u@gmail.com>
2025-03-28 00:26:37 +0100

    update routing service

--- services/routing/src/server.ts
+++ services/routing/src/server.ts
@@ -3,27 +3,27 @@ import proxy from "@fastify/http-proxy";
 
 const app = fastify();
 
-app.get("/health", (req, res) => {
-    res.send({ message: "Success" });
+app.get("/health", (_, res) => {
+  res.send({ message: "Success" });
 });
 
 app.register(proxy, {
-    upstream: "http://matchmaking:8082",
-    prefix: "/matchmaking",
-})
+  upstream: "http://matchmaking",
+  prefix: "/matchmaking",
+});
 
 app.register(proxy, {
-    upstream: "http://game:8083",
-    prefix: "/game",
+  upstream: "http://game",
+  prefix: "/game",
 });
 
 app.listen(
-    { port: 8081, host: "0.0.0.0" },
-    (err: Error | null, address: string) => {
-        if (err) {
-            console.log(err);
-            process.exit(1);
-        }
-        console.log("Server listening on " + address);
-    },
+  { port: 80, host: "0.0.0.0" },
+  (err: Error | null, address: string) => {
+    if (err) {
+      console.log(err);
+      process.exit(1);
+    }
+    console.log(`Server·listening·on·${address}`);
+  },
 );
