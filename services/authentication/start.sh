87efa1d69750a6cc79781799b5962b5780fbc674
Milana <94888u@gmail.com>
2025-03-28 16:55:55 +0100

    delete old files

--- services/authentication/start.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-
-docker build . -t transcendence_auth
-docker run -p 8080:8080 -it transcendence_auth:latest
--- services/game/start.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-
-docker build . -t transcendence_game
-docker run -p 8080:8080 -it transcendence_game:latest
