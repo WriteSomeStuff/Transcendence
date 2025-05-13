500cf32a6fb18c3727a079d42d95789cba3fad5f
Oliver <ovanrheenen@gmail.com>
2025-05-13 15:13:51 +0200

    fix: change directory structure for clarity between public pages and scripts

---
+++ frontend/public/scripts/updateAvatar.ts
@@ -0,0 +1,14 @@
+import { updateAvatar } from "../../../backend/src/authentication/authService";
+
+document.getElementById('avatarForm')?.addEventListener('submit', async (e) => {
+	e.preventDefault();
+
+	const input = document.getElementById('avatarInput') as HTMLInputElement;
+	const file = input.files?.[0];
+	if (!file) {
+		alert("Please select an Image");
+		return;
+	}
+
+	
+});
--- frontend/public/updateAvatar.ts
+++
@@ -1,3 +0,0 @@
-import { updateAvatar } from "../../backend/src/authentication/authService";
-
-const avatarInput = document.getElementById('avatarForm');
