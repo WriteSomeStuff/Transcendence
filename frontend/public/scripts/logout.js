66add5e78982ffb45554fa84987d4e609fd42403
Oliver <ovanrheenen@gmail.com>
2025-05-15 18:35:22 +0200

    feat: move logout functionality to separate script for better organization

---
+++ frontend/public/scripts/logout.js
@@ -0,0 +1,16 @@
+document.getElementById('logoutButton').addEventListener('click', function() {
+	fetch('/logout', { method: 'DELETE' })
+		.then(response => {
+			if (response.ok) {
+				window.location.href = '/index.html';
+			} else {
+				alert('Logout failed.');
+			}
+		})
+		.catch(error => {
+			console.error('Error:', error);
+			alert('An error occurred.');
+		});
+});
+
+// TODO: is it allowed to wrtie js or should it be ts as well?
