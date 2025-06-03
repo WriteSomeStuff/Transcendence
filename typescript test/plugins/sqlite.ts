c3cd88671a7fde1231377254d2e3136e593e829c
tknibbe <tymonknibbe@gmail.com>
2025-06-03 15:16:47 +0200

    removed testdirectories and renamed the memory_game dir

--- typescript test/plugins/sqlite.ts
+++
@@ -1,8 +0,0 @@
-import { FastifyInstance, FastifyPluginAsync } from "fastify"
-import db from '../db'
-
-const sqlitePlugin: FastifyPluginAsync = async (fastify: FastifyInstance) => {
-	fastify.decorate('db', db)
-}
-
-export default sqlitePlugin
--- typescript test/types/fastify.d.ts
+++
@@ -1,8 +0,0 @@
-import 'fastify'
-import sqlite3 from 'sqlite3'
-
-declare module 'fastify' {
-  interface FastifyInstance {
-    db: sqlite3.Database
-  }
-}
