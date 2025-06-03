3aa75c74dab0a4abc2dfaeff55195ed9d828eab4
Cheyenne <cheyennebune@gmail.com>
2025-06-03 17:07:28 +0200

    Change to Tailwind CSS

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -2,8 +2,35 @@
 module.exports = {
   content: ["index.html"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        rainbow: "rainbow 20s infinite"
+      },
+      keyframes: {
+        rainbow: {
+          '0%':   { borderColor: 'red', boxShadow: '0 0 20px red' },
+          '10%':  { borderColor: 'orange', boxShadow: '0 0 20px orange' },
+          '20%':  { borderColor: 'yellow', boxShadow: '0 0 20px yellow' },
+          '30%':  { borderColor: 'lime', boxShadow: '0 0 20px lime' },
+          '40%':  { borderColor: 'green', boxShadow: '0 0 20px green' },
+          '50%':  { borderColor: 'cyan', boxShadow: '0 0 20px cyan' },
+          '60%':  { borderColor: 'blue', boxShadow: '0 0 20px blue' },
+          '70%':  { borderColor: 'indigo', boxShadow: '0 0 20px indigo' },
+          '80%':  { borderColor: 'violet', boxShadow: '0 0 20px violet' },
+          '90%':  { borderColor: 'pink', boxShadow: '0 0 20px pink' },
+          '100%': { borderColor: 'red', boxShadow: '0 0 20px red' },
+        }
+      },
+      fontFamily: {
+        comic: ['Comic', 'sans-serif'],
+      },
+      backgroundImage: {
+        'stars': "url('/frontend/assets/backgrounds/movingStars.gif')",
+      }
+    },
+    backgroundSize: {
+      '20%': '20%',
+    }
   },
   plugins: [],
 }
-
