e76f3a4c1aceede4e75eaf76dc4f1f2de0924fad
tknibbe <tymonknibbe@gmail.com>
2025-06-10 17:27:23 +0200

    added leaveRoom function

---
+++ match_making/src/backend/ts/leaveRoom.ts
@@ -0,0 +1,30 @@
+
+import { roomQueues, Room } from "./room";
+import { GameMode } from "./types";
+
+export function leaveRoom(userID: number){
+	for (const gameMode in roomQueues)
+	{
+		const rooms = roomQueues[gameMode as GameMode];
+
+		for (const room of rooms) {
+			if (room.playerList.includes(userID))
+			{
+				//remove player from playerlist
+				room.playerList = room.playerList.filter((iD) => iD !== userID);
+				room.amountPlayersInRoom--;
+				console.log(room)
+				if (room.amountPlayersInRoom == 0)
+				{
+					//remove room if its empty
+					roomQueues[gameMode as GameMode] = roomQueues[gameMode as GameMode].filter((remove: Room) => remove !== room);
+				}
+			}
+		}
+	}
+
+	console.log(userID + " has chosen to leave the room");
+	console.log(roomQueues);
+}
+
+//TODO: make frontend button to test this!
