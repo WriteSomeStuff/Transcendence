ce0f87954b5aaff5250b75a0182ac0d605b81273
mstegema <m.stegeman@me.com>
2025-06-26 12:27:01 +0200

    fix: correct API endpoint URLs in fetchUserIdByUsername and fetchUsernameByUserId functions

--- backend/authentication/src/helpers/authServiceHelpers.ts
+++ backend/authentication/src/helpers/authServiceHelpers.ts
@@ -1,5 +1,5 @@
 export const fetchUserIdByUsername = async (username: string): Promise<number> => {
-	const url = process.env.USER_SERVICE_URL + '/get-username?username=' + encodeURIComponent(username);
+	const url = process.env.USER_SERVICE_URL + '/get-userid?username=' + encodeURIComponent(username);
 	const response = await fetch(url, {
 		method: 'GET',
 		headers: {
@@ -17,7 +17,7 @@ export const fetchUserIdByUsername = async (username: string): Promise<number> =
 }
 
 export const fetchUsernameByUserId = async (userId: number): Promise<string> => {
-	const url = process.env.USER_SERVICE_URL + '/get-userid?userId=' + encodeURIComponent(userId);
+	const url = process.env.USER_SERVICE_URL + '/get-username?userId=' + encodeURIComponent(userId);
 	const response = await fetch(url, {
 		method: 'GET',
 		headers: {
