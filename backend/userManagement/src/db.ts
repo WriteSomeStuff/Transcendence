337f3f2d97689ce0ef9a42b8e66379f650959188
Cheyenne <cheyennebune@gmail.com>
2025-07-02 14:56:46 +0200

    Add tables for match history

--- backend/userManagement/src/db.ts
+++ backend/userManagement/src/db.ts
@@ -24,6 +24,35 @@ const sql = `
 		account_status	TEXT	DEFAULT ('offline')	CHECK(account_status IN ('online', 'offline'))
 	);
 
+	CREATE TABLE IF NOT EXISTS tournament (
+		tournament_id		INTEGER PRIMARY KEY,
+		tournament_name		TEXT	NOT NULL	UNIQUE,
+		created_at			TEXT	DEFAULT (datetime('now', '+2 hour')),
+		tournament_end		TEXT,
+		tournament_status	TEXT	DEFAULT ('ongoing')	CHECK(tournament_status IN ('ongoing', 'finished'))
+	);
+
+	CREATE TABLE IF NOT EXISTS match_state (
+		match_id		INTEGER PRIMARY KEY,
+		match_date		TEXT	DEFAULT (datetime('now', '+2 hour')),
+		match_status	TEXT	DEFAULT ('ongoing')	CHECK(match_status IN ('ongoing', 'finished')),
+		match_end		TEXT,
+		tournament_id	INTEGER,
+		FOREIGN KEY (tournament_id)
+			REFERENCES tournament (tournament_id)
+	);
+
+	CREATE TABLE IF NOT EXISTS match_participant (
+		user_id		INTEGER		NOT NULL,
+		match_id	INTEGER		NOT NULL,
+		score		INTEGER		DEFAULT (0),
+		PRIMARY KEY (user_id,match_id),
+		FOREIGN KEY (user_id)
+			REFERENCES user (user_id),
+		FOREIGN KEY (match_id)
+			REFERENCES match_state (match_id)
+	);
+
 	CREATE TRIGGER IF NOT EXISTS update_last_login
 	AFTER UPDATE OF account_status ON user
 	WHEN NEW.account_status = 'online'
