87bcdf3041aa22d05ec4834714c9f4a1788c841a
Oliver <ovanrheenen@gmail.com>
2025-07-10 15:42:09 +0200

    refactor: remove unused Friend interface from types.ts

--- backend/user_management/src/types/types.ts
+++
@@ -1,7 +0,0 @@
-// TODO use shared schema user object instead of this interface
-export interface Friend {
-	friendshipId:	number;
-	userId:			number;
-	username:		string;
-	accountStatus:	string;
-}
